f88440ae6fae899d49abf390e8165216a9fea2c8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-06 09:18:43 +0200

    Schema creation

---
+++ backend/schema/SalarySchema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import connect from "../db/connection.js";
+
+connect();
+
+const SalarySchema = mongoose.Schema({
+    GlossSalary: { type: Number, required: true },
+    TotalDeduction: { type: Number, required: true },
+    NetSalary: { type: Number, required: true },
+    month: { type: Date, required: true },
+}, { timestamps: true });
+
+mongoose.model("employees", EmployeeSchema);
