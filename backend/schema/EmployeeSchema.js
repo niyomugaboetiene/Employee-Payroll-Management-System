8e32c25ac89c0142d22e1d48e27bd13daf0fc3dc
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-06 09:16:03 +0200

    Schema creation

--- backend/schema/EmployeeSchema.js
+++ backend/schema/EmployeeSchema.js
@@ -4,5 +4,12 @@ import connect from "../db/connection.js";
 connect();
 
 const EmployeeSchema = mongoose.Schema({
-    employeeNumber: { type: Number, }
-})
+    employeeNumber: { type: Number, unique: true, required: true },
+    FirstName: { type: String, required: true },
+    LastName: { type: String, required: true },
+    Position: { type: String, required: true },
+    Address: { type: String, required: true },
+    Telephone: { type: String , required: true },
+    Gender: { type: String, required: true },
+    hiredDate: { type: Date, required: true },
+}, { timestamps: true })
