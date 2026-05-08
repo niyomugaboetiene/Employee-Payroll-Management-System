d66e0eb0c921f23e85905682b5c531ec9db70197
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-08 13:40:32 +0200

    implement SalaryRoute.js

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import EmployeeRoute from "./routes/EmployeeRoute.js";
+import SalaryRuute from './routes/SalaryRoute.js';
+import DepartmentRoute from "./routes/DepartmentRoute.js";
+
 import cors from "cors";
 
 const app = express();
@@ -10,6 +13,8 @@ app.use(cors({
 }));
 
 app.use('/employee', EmployeeRoute);
+app.use('/salary', SalaryRuute);
+app.use('/department', DepartmentRoute);;
 
 app.listen(4000, () => {
     console.log('http://localhost:4000');
