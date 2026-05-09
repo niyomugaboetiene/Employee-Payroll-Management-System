038b7d76ed41ede966f4d1699afa4581be6f70c8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-09 10:15:41 +0200

    implement UpdateSalary.jsx

---
+++ frontend/src/pages/employee/EmployeeList.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+
+const handleGetEmployee = () => {
+    const [employee, setEmployee] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+}
