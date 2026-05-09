b1adfaa203cd4616b4ebd5225af367527d629baa
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-09 10:09:21 +0200

    implement AddSalary.jsx

--- frontend/src/pages/salary/AddSalary.jsx
+++ frontend/src/pages/salary/AddSalary.jsx
@@ -122,7 +122,7 @@ const AddSalary = () => {
                     <button className="bg-red-500 text-xs px-2 rounded-lg font-light text-white hover:bg-red-400 transition-colors active:bg-red-500">Back</button>
                 </div> */}
               <div className="mt-4">
-                    <button onClick={handleAddNewDepartment} className="w-full bg-gray-300 py-2 text-xs px-2 rounded-lg font-light hover:bg-gray-400 transition-colors active:bg-gray-500">+ Add new</button>
+                    <button onClick={handleAddNewSalary} className="w-full bg-gray-300 py-2 text-xs px-2 rounded-lg font-light hover:bg-gray-400 transition-colors active:bg-gray-500">+ Add new</button>
                     <button className="bg-red-500 text-xs px-2 w-full py-2 mt-2 rounded-lg font-light text-white hover:bg-red-400 transition-colors active:bg-red-500">Back</button>
                 </div>
             </div>
