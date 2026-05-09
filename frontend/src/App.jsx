1fe64fcf5f52f4940ce25f772b95a85224f82e78
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-09 09:49:18 +0200

    implement App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import AddEmployee from "./pages/employee/AddEmployee"
+import AddDepartment from "./pages/department/AddDepartment"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
 
   return (
-    <>
-     <AddEmployee />
-    </>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/AddEmployee" element={<AddEmployee />}/>
+          </Routes>
+      </BrowserRouter>
   )
 }
 
