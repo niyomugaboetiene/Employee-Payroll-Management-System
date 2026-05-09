c071545b96085ba459a4bfa9ecba19bf044fd0b7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-09 10:16:56 +0200

    implement UpdateSalary.jsx

--- frontend/src/pages/employee/EmployeeList.jsx
+++ frontend/src/pages/employee/EmployeeList.jsx
@@ -1,6 +1,13 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 const handleGetEmployee = () => {
     const [employee, setEmployee] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+
+    const handleGetEmployee = async () => {
+        try {
+            const res = await axios.get('http://localhost:4000/employee/employeeList')
+        }
+    }
 }
