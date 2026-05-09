48c2d2b48afced523f8f1262deb082e7bd485e3e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-09 10:43:36 +0200

    implement App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import AddEmployee from "./pages/employee/AddEmployee"
 import AddDepartment from "./pages/department/AddDepartment"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AddSalary from "./pages/salary/AddSalary"
+import EmployeeList from "./pages/employee/EmployeeList"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
               <Route path="/AddEmployee" element={<AddEmployee />} />
               <Route path="/AddDepartment" element={<AddDepartment />} />
               <Route path="/AddSalary" element={<AddSalary />} />
+              <Route path="/employeeList" element={<EmployeeList />} />
           </Routes>
       </BrowserRouter>
   )
