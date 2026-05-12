ebb1b0e08ea3f8ff7a7251238b8b11efecafa244
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-12 12:14:40 +0200

    Adding more features

--- frontend/src/pages/dashboard/Nav.jsx
+++ frontend/src/pages/dashboard/Nav.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <div className="bg-gray-100 min-h-screen">
             <div className="bg-white h-14">
                 <div className="relative">
-                    <p className="absolute top-4 left-12 text-3xl font-bold bg-linear-to-b from-green-300 to-gray-600 text-transparent bg-clip-text">EMPS</p>
+                    <p className="absolute top-4 left-12 text-3xl font-bold bg-linear-to-b from-green-300 to-gray-600 text-transparent bg-clip-text hover:scale-105 transition duration-300">EMPS</p>
                 </div>
                 <nav className="flex justify-center gap-6">
                     <Link>Home</Link>
