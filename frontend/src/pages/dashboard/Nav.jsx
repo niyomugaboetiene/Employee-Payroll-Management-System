ab2d8136120835e699bf4ace863ff7c074663778
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-12 12:13:02 +0200

    Adding more features

--- frontend/src/pages/dashboard/Nav.jsx
+++ frontend/src/pages/dashboard/Nav.jsx
@@ -1,11 +1,14 @@
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
     
 
     return (
-        <div>
-            <div>
-                <nav>
+        <div className="bg-gray-100 min-h-screen">
+            <div className="bg-white h-14">
+                <div className="relative">
+                    <p className="absolute top-4 left-12 text-3xl font-bold bg-linear-to-b from-green-300 to-gray-600 text-transparent">EMPS</p>
+                </div>
+                <nav className="flex justify-center gap-6">
                     <Link>Home</Link>
                     <Link>Employees</Link>
                     <Link>Department</Link>
