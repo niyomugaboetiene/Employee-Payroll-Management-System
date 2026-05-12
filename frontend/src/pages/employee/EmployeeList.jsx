b7c6be17c353403fad592d88da1e16828e654b6c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-12 08:24:43 +0200

    Adding more features

--- frontend/src/pages/employee/EmployeeList.jsx
+++ frontend/src/pages/employee/EmployeeList.jsx
@@ -41,7 +41,10 @@ const EmployeeList = () => {
     return (
         <div className="bg-gray-50 min-h-screen flex justify-center">
             <div className="mt-12">
-                <h1 className="text-center mb-2 text-2xl font-bold text-gray-700">Employee List</h1>
+                <div className="flex justify-between mb-3">
+                   <h1 className="mb-2 text-2xl font-bold text-gray-700">Department List</h1>
+                   <button onClick={() => navigate('/AddDepartment')} className="bg-gray-400 text-white px-3 rounded-lg hover:bg-gray-500 transition-colors">Add Department</button>
+                </div>
                 <table border={2}>
                     <thead className="bg-gray-400 text-white ">
                         <tr>
