ce7248e278e1d9b412fcb9ddae522e782dfe18d0
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-12 12:04:14 +0200

    Adding more features

---
+++ frontend/src/pages/dashboard/Nav.jsx
@@ -0,0 +1,20 @@
+
+const NavBar = () => {
+    
+
+    return (
+        <div>
+            <div>
+                <nav>
+                    <Link>Home</Link>
+                    <Link>Employees</Link>
+                    <Link>Department</Link>
+                    <Link>Salary</Link>
+                    <Link>Payroll Report</Link>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
