21b332e3e145913a1b8ec6a6e45ade9e72d676e1
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-13 08:49:42 +0200

    Adding more features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,47 @@
-// import AddEmployee from "./pages/employee/AddEmployee"
-// import AddDepartment from "./pages/department/AddDepartment"
-// import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import AddSalary from "./pages/salary/AddSalary"
-// import EmployeeList from "./pages/employee/EmployeeList"
-// import UpdateEmployee from "./pages/employee/UpdateEmployee"
-// import DepartmentList from "./pages/department/DepartmentLIst"
-// import UpdateDepartment from "./pages/department/UpdateDepartment"
-// import SalaryList from "./pages/salary/SalaryList"
-// import UpdateSalary from "./pages/salary/UpdateSalary"
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ListComponent from "./pages/employee/List"
 import AddEmployee from "./pages/employee/AddEmployee"
-import UpdateEmployee from "./pages/employee/Update"
+import AddDepartment from "./pages/department/AddDepartment"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AddSalary from "./pages/salary/AddSalary"
+import EmployeeList from "./pages/employee/EmployeeList"
+import UpdateEmployee from "./pages/employee/UpdateEmployee"
+import DepartmentList from "./pages/department/DepartmentLIst"
+import UpdateDepartment from "./pages/department/UpdateDepartment"
+import SalaryList from "./pages/salary/SalaryList"
+import UpdateSalary from "./pages/salary/UpdateSalary"
+import NavBar from "./pages/dashboard/Nav"
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import ListComponent from "./pages/employee/List"
+// import AddEmployee from "./pages/employee/AddEmployee"
+// import UpdateEmployee from "./pages/employee/Update"
 
 function App() {
 
   return (
-      // <BrowserRouter>
-      //     <Routes>
-      //         <Route path="/AddEmployee" element={<AddEmployee />} />
-      //         <Route path="/AddDepartment" element={<AddDepartment />} />
-      //         <Route path="/AddSalary" element={<AddSalary />} />
-      //         <Route path="/employeeList" element={<EmployeeList />} />
-      //         <Route path="/employee/update/:_id" element={<UpdateEmployee />} />
-      //         <Route path="/department/departmentList" element={<DepartmentList />}/>
-      //         <Route path="/department/update/:_id" element={<UpdateDepartment />}/>
-      //         <Route path="/salary/SalaryList" element={<SalaryList />}/>
-      //         <Route path="/salary/update/:_id" element={<UpdateSalary />}/>
+      <BrowserRouter>
+      <NavBar />
+          <Routes>
+              {/* <Route path="/" element={} /> */}
+              <Route path="/AddEmployee" element={<AddEmployee />} />
+              <Route path="/AddDepartment" element={<AddDepartment />} />
+              <Route path="/AddSalary" element={<AddSalary />} />
+              <Route path="/employeeList" element={<EmployeeList />} />
+              <Route path="/employee/update/:_id" element={<UpdateEmployee />} />
+              <Route path="/department/departmentList" element={<DepartmentList />}/>
+              <Route path="/department/update/:_id" element={<UpdateDepartment />}/>
+              <Route path="/salary/SalaryList" element={<SalaryList />}/>
+              <Route path="/salary/update/:_id" element={<UpdateSalary />}/>
 
-      //     </Routes>
-      // </BrowserRouter>
+          </Routes>
+      </BrowserRouter>
 
-    <BrowserRouter>
-        <Routes>
-            <Route path="/AddEmployee" element={<AddEmployee />} />
-            <Route path="/empList" element={<ListComponent />}/>
-            <Route path="/update/:_id" element={<UpdateEmployee />}/>
-        </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //     <Routes>
+    //         <Route path="/AddEmployee" element={<AddEmployee />} />
+    //         <Route path="/empList" element={<ListComponent />}/>
+    //         <Route path="/update/:_id" element={<UpdateEmployee />}/>
+    //     </Routes>
+    // </BrowserRouter>
   )
 }
 
