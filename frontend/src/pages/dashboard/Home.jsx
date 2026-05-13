a39b8dfbb53a3061e510f7b67da078be2c920164
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-13 08:55:24 +0200

    Adding more features

--- frontend/src/pages/dashboard/Home.jsx
+++ frontend/src/pages/dashboard/Home.jsx
@@ -3,9 +3,15 @@ const HomePage = () => {
 
     return (
         <div>
-            <div>
+            <div className="mt-30 ms-4 text-2xl font-light text-gray-800">
                 <h1>Welcome to Employee paylor management system</h1>
+
+                <div>
+                    <button>Login</button>
+                </div>
             </div>
         </div>
     )
 }
+
+export default HomePage;
