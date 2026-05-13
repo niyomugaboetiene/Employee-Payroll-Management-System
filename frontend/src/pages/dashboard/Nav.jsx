0893999b5b636b30ee739f6851fd93ed98790120
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-13 08:44:21 +0200

    Adding more features

--- frontend/src/pages/dashboard/Nav.jsx
+++ frontend/src/pages/dashboard/Nav.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
 
     return (
         <div className="bg-gray-100 min-h-screen">
-            <div className="bg-white h-18 flex justify-between">
+            <div className="bg-white h-18 flex justify-between fixed top-0 left-0 right-0">
                 <div className="relative">
                     <p className="absolute sm:left-0 lg:top-4 md:top-4 md:left-12 text-3xl font-bold bg-linear-to-b from-green-300 to-gray-600 text-transparent bg-clip-text hover:scale-105 transition duration-300">EMPS</p>
                 </div>
@@ -13,7 +13,7 @@ const NavBar = () => {
                     <Link className="hover:border-b border-gray-600" to={'/employeeList'}>Employees</Link>
                     <Link className="hover:border-b border-gray-600" to={'/department/departmentList'}>Department</Link>
                     <Link className="hover:border-b border-gray-600" to={'/salary/SalaryList'}>Salary</Link>
-                    <Link className="hover:border-b border-gray-600" to={'/report'}>Payroll Report</Link>
+                    <Link className="hover:border-b border-gray-600" to={'/eport'}>Payroll Report</Link>
                 </nav>
                 <div className="flex  sm:space-x-1 me-4">
                     <button className="bg-gray-200 h-8 mt-5 px-4 rounded-s-full hover:bg-gray-300 transition-colors text-gray-700">Sign in</button>
