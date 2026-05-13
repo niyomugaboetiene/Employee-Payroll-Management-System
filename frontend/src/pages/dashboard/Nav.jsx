be006794c3ffd670c39800d83a2f9ebbf81b1913
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-13 08:36:24 +0200

    Adding more features

--- frontend/src/pages/dashboard/Nav.jsx
+++ frontend/src/pages/dashboard/Nav.jsx
@@ -9,15 +9,15 @@ const NavBar = () => {
                     <p className="absolute sm:left-0 lg:top-4 md:top-4 md:left-12 text-3xl font-bold bg-linear-to-b from-green-300 to-gray-600 text-transparent bg-clip-text hover:scale-105 transition duration-300">EMPS</p>
                 </div>
                 <nav className="sm:gap-1 gap-2 flex justify-center flex-wrap md:gap-6 pt-4 text-xl font-light text-gray-700">
-                    <Link>Home</Link>
-                    <Link>Employees</Link>
-                    <Link>Department</Link>
-                    <Link>Salary</Link>
-                    <Link>Payroll Report</Link>
+                    <Link className="hover:border-b border-gray-600">Home</Link>
+                    <Link className="hover:border-b border-gray-600">Employees</Link>
+                    <Link className="hover:border-b border-gray-600">Department</Link>
+                    <Link className="hover:border-b border-gray-600">Salary</Link>
+                    <Link className="hover:border-b border-gray-600">Payroll Report</Link>
                 </nav>
                 <div className="flex  sm:space-x-1 me-4">
-                    <button className="bg-gray-200 h-8 mt-5 px-4 rounded-s-full">Sign in</button>
-                   <button className="border h-8 mt-5 px-3 border-gray-500 rounded-e-full">Sign up</button>
+                    <button className="bg-gray-200 h-8 mt-5 px-4 rounded-s-full hover:bg-gray-300 transition-colors text-gray-700">Sign in</button>
+                   <button className="border h-8 mt-5 px-3 border-gray-500 rounded-e-full font-light text-gray-700 hover:bg-gray-300 transition-colors hover:text-white">Sign up</button>
                 </div>
             </div>
 
