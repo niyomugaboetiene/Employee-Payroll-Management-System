bf5a366267da56c9d23667605d6fc8890eeb345f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-13 09:15:02 +0200

    Adding more features

--- frontend/src/pages/dashboard/Home.jsx
+++ frontend/src/pages/dashboard/Home.jsx
@@ -1,6 +1,21 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
+    const [total, setTotal] = useState(null);
 
+    const handleGetTotal = async () => {
+          try {
+            const res = await axios.get('http://localhost:4000/department/total');
+            setTotal(res.data.total);
+          } catch (err) {
+            console.error(err);
+          }
+    }
+
+    useEffect(() => {
+        handleGetTotal();
+    }, []);
     return (
         <div>
             <div className="mt-30 ms-4 text-2xl font-light text-gray-800">
