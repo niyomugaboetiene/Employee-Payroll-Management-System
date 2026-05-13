deabf696ec7d078e6dca57b3d46ad5930d358ad9
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-13 09:57:47 +0200

    Adding more features

--- frontend/src/pages/dashboard/Home.jsx
+++ frontend/src/pages/dashboard/Home.jsx
@@ -156,6 +156,37 @@ const HomePage = () => {
                          <Link className="flex justify-center text-blue-400 font-black underline" to={'/department/departmentList'}>View more</Link>
                      </div>
                      </div>
+                     
+                     <div className="w-410 mt-8 text-lg bg-gray-200 rounded-lg  p-4">
+                    <h1 className="font-bold text-gray-700 text-2xl mb-3">Recent Department</h1>
+                     <table border={2} className="w-404">
+                    <thead className="bg-gray-400 text-white ">
+                        <tr>
+                            <th className="py-3 px-0">Departement Code</th>
+                            <th className="py-3 px-0">Departement Name</th>
+                            <th className="py-3 px-0">Gross Salary</th>
+                            <th className="py-3 px-0">Created At</th>
+                            <th className="py-3 px-0">Last Update</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {recentDepartment?.map((dep, index) => (
+                            <tr key={index} className={`${index % 2 === 0 ? 'bg-gray-100 hover:bg-gray-200' : 'bg-gray-300 hover:bg-gray-400'}`}>
+                                <td className="py-3 px-5">{dep.DepartementCode}</td>
+                                <td className="py-3 px-5">{dep.DepartementName}</td>
+                                <td className="py-3 px-5">{dep.GrossSalary}</td>
+                                <td className="py-3 px-5">{new Date(dep.createdAt).toLocaleDateString()}</td>
+                                <td className="py-3 px-5">{new Date(dep.updatedAt).toLocaleDateString()}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+               
+                     <div className="mt-3">
+                         <Link className="flex justify-center text-blue-400 font-black underline" to={'/department/departmentList'}>View more</Link>
+                     </div>
+                     </div>
                 </div>
                 
             </div>
