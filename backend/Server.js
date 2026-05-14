823fad756b66d27a7e936da76a99bf387e9c325e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-14 10:50:15 +0200

    Implementing admin route

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,7 @@ import SalaryRuute from './routes/SalaryRoute.js';
 import DepartmentRoute from "./routes/DepartmentRoute.js";
 import AdminRouter from "./routes/AdmibRoute.js";
 
+import session from "express-session";
 import cors from "cors";
 
 const app = express();
@@ -12,6 +13,10 @@ app.use(cors({
     origin: 'http://localhost:5173',
     // credentials: true
 }));
+
+app.use(session({
+    secret: 'my-secret-key'
+}))
 
 app.use('/employee', EmployeeRoute);
 app.use('/salary', SalaryRuute);
