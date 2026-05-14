7d84eb54eba5f880565149bb5d2547fd3d68cba3
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-14 11:21:15 +0200

    implementing admin auth

--- frontend/src/pages/admin/Login.jsx
+++ frontend/src/pages/admin/Login.jsx
@@ -21,14 +21,14 @@ const Login = () => {
                 return;
             }
             setLoading(true);
-            const res = await axios.post('http://localhost:4000/department/addDepartment', {
-                DepartementCode, DepartementName, GrossSalary
+            const res = await axios.post('http://localhost:4000/admin/login', {
+                username, password
             });
            setLoading(false);
            setError("");
            setSuccess(res.data.message);
           setTimeout(() => {
-                    setSuccess("");
+                setSuccess("");
          }, 4000);
         } catch (err) {
             console.error(err);
@@ -46,7 +46,7 @@ const Login = () => {
     return (
         <div className="bg-gray-200 min-h-screen flex justify-center items-center">
             <div className="bg-white p-3 rounded-lg shadow-2xl w-90">
-                <h2 className="text-gray-700 font-bold text-center text-sm">Add Department Portal</h2>
+                <h2 className="text-gray-700 font-bold text-center text-sm">Login Portal</h2>
 
                 {success && (
                     <div className="bg-green-500 mt-2 mb-3 p-1 rounded-lg text-green-100">
