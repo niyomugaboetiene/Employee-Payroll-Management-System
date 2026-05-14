c3902611f06b5c7cc9813aa2f273aa832e035223
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-14 10:26:22 +0200

    Implementing admin route

--- backend/routes/AdmibRoute.js
+++ backend/routes/AdmibRoute.js
@@ -1,10 +1,17 @@
 import Admin from "../schema/AdminSchema.js";
 import express from "express";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
     try {
+        const { username, password } = req.body;
+
+        if (!password || !username) {
+            return res.status(403).json({ message: 'Fill out missing fields' });
+        }
+
         
     }
 })
