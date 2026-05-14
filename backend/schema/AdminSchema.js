27da8d70d61aa5b353894befb68ff447bff4ab5a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-14 10:23:12 +0200

    Implementing admin schema

--- backend/schema/AdminSchema.js
+++ backend/schema/AdminSchema.js
@@ -2,3 +2,11 @@ import mongoose from "mongoose";
 import connection from "../db/connection";
 
 connection();
+
+const AdminSchema = mongoose.Schema({
+       username: { type: String, required: true, unique: true },
+       password: { type: String, required: true }
+}, { timestamps: true});
+
+const Admin = mongoose.model("admins", AdminSchema);
+export default Admin;
