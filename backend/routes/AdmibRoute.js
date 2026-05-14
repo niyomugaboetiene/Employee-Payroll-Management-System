ff39c648f0c7dfba752bef2c0f64172d87523aa6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-14 10:24:22 +0200

    Implementing admin route

--- backend/routes/AdmibRoute.js
+++ backend/routes/AdmibRoute.js
@@ -1,1 +1,10 @@
 import Admin from "../schema/AdminSchema.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        
+    }
+})
