fcccb545bc881a8952cbefd670d4469a3efc8b04
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-16 08:53:51 +0200

    Creating footer

--- frontend/src/pages/dashboard/Footer.jsx
+++ frontend/src/pages/dashboard/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <div className="m-2 flex">
          <div className="fixed top-190 h-full bg-gray-200 w-full py-4 p-3">
-            <div className="flex">
+            <div className="flex justify-between">
             <div>
                 <h1>EMPS</h1>
             </div>
