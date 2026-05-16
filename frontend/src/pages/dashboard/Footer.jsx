9d55b63c51a44a7521fe06ee03050104c0615312
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-16 08:45:36 +0200

    Creating footer

--- frontend/src/pages/dashboard/Footer.jsx
+++ frontend/src/pages/dashboard/Footer.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <div>
-
+         <div>
+            <div>
+                <h1>EMPS</h1>
+            </div>
+            <div>
+                <Link></Link>
+            </div>
+         </div>
         </div>
     )
 }
